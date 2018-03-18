65547a96aef297ac19f8302ddadd5effd6ec4830
magatamaX <hizakitomohiro.r@gmail.com>
2018-03-19 04:56:53 +0900

    5th

--- src/index.js
+++ src/index.js
@@ -1,28 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// SuperAgentの利用を宣言
 import request from 'superagent';
+
+// Path
 const path = require('path');
 
+
+// サムネイルコンポーネント
 class Gallery extends React.Component {
 
   constructor(props) {
     super(props);
+    // ステート初期化
     this.state = {
-      items: [],
+      items: null,
+      length: 0,
       currentPage: 0,
-      itemsPerPage: {
-        pc: 7,
-        sp: 3,
-      },
-      totalPages: {
-        pc: 0,
-        sp: 0,
-      }
+      itemsPerPage: 0,
+      totalPages: 0,
+      itemWidth: 0,
+      itemHeight: 0,
+      topPartsWidth: null,
+      topPartsHeight: null,
     }
   }
 
+  getWindowSize() {
+    var w = window,
+          d = document,
+          e = d.documentElement,
+          g = d.getElementsByTagName('body')[0],
+          w = e.clientWidth || g.clientWidth || w.innerWidth,
+          h = e.clientHeight|| g.clientHeight || w.innerHeight;
+
+    return {
+      width: w,
+      height: h
+    }
+  }
+
+
+  // マウントされるとき
   componentWillMount () {
+
+    // windowSize取得
+    window.addEventListener('load', () => {
+      var size = this.getWindowSize()
+      // console.log('your viewport size', size)
+      this.onResize( size )
+    })
+    window.addEventListener('resize', () => {
+      var size = this.getWindowSize()
+      // console.log('your viewport size', size)
+      this.onResize( size )
+    })
+
+
     const jsonPath = path.resolve(__dirname, 'test.json')
+
+    // JSONデータ読み込み
     request.get(jsonPath)
       .accept('application/json')
       .end( ( err, res ) => {
@@ -30,28 +68,84 @@ class Gallery extends React.Component {
       })
   }
 
+  // データを読み込んだとき
   loadedJSON ( err, res ) {
     if ( err ) {
-      console.log(err)
+      console.log('データ読み込みに失敗しました。')
       return
     }
 
+    // ステート更新
     this.setState({
       items: [res.body],
-      totalPages: {
-        pc: Math.ceil(res.body.img.length / 8),
-        sp: Math.ceil(res.body.img.length / 4),
-      }
+      length: res.body.img.length,
     })
+
+    // console.log(this.state.items)
+
+    this.setImageSize();
+  }
+
+
+  setImageSize () {
+
+    const tgt = this.state.items[0].img
+
+      var img = [], width, height, ratio
+    for ( let i=0; i<tgt.length; i++) {
+
+
+          img[i] = new Image()
+          img[i].src = tgt[i].thumblarge
+          img[i].width = img[i].width;  // 幅
+          img[i].height = img[i].height; // 高さ
+
+          if ( ( img[i].width / img[i].height ) > 1 ) {
+            //横長Landscape
+            tgt[i].orientation = 'ls'
+          } else if ( ( img[i].width / img[i].height ) <= 1 ) {
+            //縦長Portrait
+            tgt[i].orientation = 'pr'
+          }
+
+    }
 
   }
 
   onPageChange( index ) {
-    console.log( index );
+    // console.log( index );
     this.setState({
       currentPage: index,
     })
   }
+
+  onResize ( size ) {
+
+    if( size.width >= 920 ){
+      this.setState({
+        itemsPerPage: 7,
+        totalPages: Math.ceil( this.state.length / 8 ),
+        itemWidth: (920 / 4) + 'px',
+        itemHeight: (920 / 4) + 'px',
+      })
+    } else if ( size.width >= 600 && size.width < 920 ){
+      this.setState({
+        itemsPerPage: 7,
+        totalPages: Math.ceil( this.state.length / 8 ),
+        itemWidth: (size.width / 4) + 'px',
+        itemHeight: (size.width / 4) + 'px',
+      })
+    } else if ( size.width < 600 ) {
+      this.setState({
+        itemsPerPage: 3,
+        totalPages: Math.ceil( this.state.length / 4 ),
+        itemWidth: (size.width / 2) + 'px',
+        itemHeight: (size.width / 2) + 'px',
+      })
+    }
+
+  }
+
 
   render () {
 
@@ -60,27 +154,26 @@ class Gallery extends React.Component {
         現在読み込み中</div>
     }
 
-    const photoList = this.state.items.map( e => {
+    const photoList = this.state.items.map( ( e, index ) => {
 
       if ( e.result === 'NG') {
 
-        return <div className="NG">
-          <p>エラーです</p>
-          <p>{e.err_cd}</p>
-          <p>{e.err_msg}</p>
+        return <div className="NG" key='result_NG'>
+          <p key='err_msg'>{e.err_msg}</p>
+          <p key='err_cd'>{e.err_cd}</p>
         </div>
 
       } else {
 
-        const items_list_count = this.state.itemsPerPage.pc;
-        console.log(items_list_count);
+        const items_list_count = this.state.itemsPerPage;
+        // console.log(items_list_count);
 
         let items_count = 0;
         let page = 0;
         let items_list = [];
         let items_list_row = [];
 
-        for ( let i=0; i <= e.img.length; i++ ) {
+        for ( let i=0; i <= this.state.length ; i++ ) {
 
           if( items_count > items_list_count ) {
 
@@ -98,9 +191,54 @@ class Gallery extends React.Component {
 
         const imgList = items_list[this.state.currentPage].map( ( images, index ) => {
 
-          return <li className="thumb" data-index={index} data-seq={images.seq}>
-            <a href={images.pageurl} target="_blank">
-              <img src={images.thumblarge} alt={images.title} />
+          const style = {
+            "overflow": 'hidden',
+            "width": this.state.itemWidth,
+            "height": this.state.itemHeight,
+            "margin": 0,
+            "boxSizing": 'border-box',
+            "padding": '0.5%',
+            "display": 'block',
+            "float": 'left',
+          }
+          const styleA = {
+            "display": 'block',
+            "width": '100%',
+            "height": '100%',
+            "overflow": 'hidden',
+          }
+          const styleLs = {
+            "height": '100%',
+            "width": 'auto',
+            "maxWidth": 'none',
+            "position": 'relative',
+            "left": '50%',
+            "top": '0',
+            "WebkitTransform": 'translateX(-50%)',
+            "MsTransform": 'translateX(-50%)',
+            "transform": 'translateX(-50%)',
+          }
+          const stylePr = {
+            "width": '100%',
+            "height": 'auto',
+            "maxHeight": 'none',
+            "position": 'relative',
+            "left": '0',
+            "top": '50%',
+            "WebkitTransform": 'translateY(-50%)',
+            "MsTransform": 'translateY(-50%)',
+            "transform": 'translateY(-50%)',
+          }
+
+          return <li style={style} key={images.nickname} className="thumb" data-index={index} data-seq={images.seq}>
+            <a href={images.pageurl} style={styleA} target="_blank">
+            {(() => {
+              if ( images.orientation == 'ls' ){
+                  return <img src={images.thumblarge} alt={images.title} style={styleLs} />
+                } else {
+                  return <img src={images.thumblarge} alt={images.title} style={stylePr} />
+                }
+              })()}
             </a>
           </li>
         })
@@ -117,7 +255,7 @@ class Gallery extends React.Component {
 
       } else {
 
-        const page_list_count  = parseInt(this.state.totalPages.pc);
+        const page_list_count  = parseInt(this.state.totalPages);
 
         let list_count = 0;
         let list_row = [];
@@ -129,7 +267,8 @@ class Gallery extends React.Component {
 
         }
 
-console.log(list_row)
+        // console.log(list_row)
+
         const hrefList = list_row.map( ( page, index ) => {
 
           let isCurrent = {};
@@ -142,10 +281,10 @@ console.log(list_row)
                 "borderColor": '#fff'
             };
             isDisabled = 'disabled'
-            console.log(isCurrent)
+            // console.log(page)
           }
 
-          return <a disabled={isDisabled} style={isCurrent} name={index} href='javascript:void(0)' onClick={ page => this.onPageChange( index ) }>{index+1}</a>
+          return <a key={page} disabled={isDisabled} style={isCurrent} name={index} href='javascript:void(0)' onClick={ page => this.onPageChange( index ) }>{index+1}</a>
         })
 
         return hrefList
@@ -156,10 +295,10 @@ console.log(list_row)
     })
 
     return (
-      <div>
-        <p className="gallery_pager clearfix">{pagination}</p>
-        <ul className='galleryTop_photoList'>{photoList}</ul>
-        <p className="gallery_pagerBottom">{pagination}</p>
+      <div key='galleryLists'>
+        <p key='galleryPager' className="gallery_pager clearfix">{pagination}</p>
+        <ul key='galleryImageList' className='galleryTop_photoList clearfix'>{photoList}</ul>
+        <p key='galleryPagerBottom' className="gallery_pagerBottom">{pagination}</p>
       </div>
     )
 
