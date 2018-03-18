c5df95993007440f8d65fe377e29ce7b2a162fc6
magatamaX <hizakitomohiro.r@gmail.com>
2018-03-18 16:28:13 +0900

    3rd

--- src/index.js
+++ src/index.js
@@ -1,23 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const request = require('superagent');
+import request from 'superagent';
+const path = require('path');
 
+class PhotoList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  componentWillMount () {
+    const jsonPath = path.resolve(__dirname, 'test.json')
+    request.get(jsonPath)
+      .accept('application/json')
+      .end( ( err, res ) => {
+        this.loadedJSON( err, res )
+      })
+  }
+
+  loadedJSON ( err, res ) {
+    if ( err ) {
+      console.log(err)
+      return
+    }
+
+    this.setState({
+      items: [res.body]
+    })
+  }
+
+  render () {
+
+    if ( !this.state.items ) {
+      return <div className='App'>
+        現在読み込み中</div>
+    }
+
+    const options = this.state.items.map( e => {
+      console.log(e)
+
+      if ( e.result === 'NG') {
+
+        return <div className="NG">
+          <p>エラーです</p>
+          <p>{e.err_cd}</p>
+          <p>{e.err_msg}</p>
+        </div>
+
+      } else {
+
+        const imgList = e.img.map( images => {
+          console.log(images);
+
+          return <li className="thumb">
+            <a href={images.pageurl} target="_blank">
+              <img src={images.thumblarge} alt={images.title} />
+            </a>
+          </li>
+        })
+
+        return imgList
+      }
+    })
+          console.log(options)
+    return (
+        <ul className='galleryTop_photoList'>{options}</ul>
+    )
+
+  }
+
+}
 
 ReactDOM.render(
-  <div>
-    <p className="gallery_pager clearfix">
-      <span>1</span>
-    </p>
-    <ul className="galleryTop_photoList">
-      <li className="thumb">
-        <a>
-          rrrrr
-        </a>
-      </li>
-    </ul>
-    <p className="gallery_pagerBottom">
-      <span>1</span>
-    </p>
-  </div>,
+  <PhotoList />,
   document.getElementById('gallery')
 );
